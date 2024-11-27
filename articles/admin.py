1980b888a6ea2b2573827cfca0bbc015eb7c7d13
yaredow <yaredyilma11@gmail.com>
2024-11-27 21:15:38 +0000

    feat: add a comment feature to the articles

--- articles/admin.py
+++ articles/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Comment
 
-# Register your models here.
 
-admin.site.register(Article)
+class CommentInline(admin.StackedInline):
+    model = Comment
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [CommentInline]
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
