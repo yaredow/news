92b4c16ce730753ac93f44d13e993864d7b7fa1d
yaredow <yaredyilma11@gmail.com>
2024-11-27 03:26:04 +0000

    finish creating and styling password forget and reset functionalities

--- config/settings.py
+++ config/settings.py
@@ -124,6 +124,7 @@ AUTH_USER_MODEL = "accounts.CustomUser"
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
